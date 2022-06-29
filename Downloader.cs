e804a2b3f48f4b4e4ff9751c9ac97d02d1cfeba5
MeysamMoghaddam <65253484+MeysamMoghaddam@users.noreply.github.com>
2022-06-29 19:47:13 +0430

    add project

---
+++ Downloader.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.JSInterop;
+namespace FileDownloader
+{
+    public class Downloader : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public Downloader(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/FileDownloader/downloader.js").AsTask());
+        }
+
+        public async Task Download(byte[] file, string fileName)
+        {
+            var fileStream = new MemoryStream(file);
+            using var streamRef = new DotNetStreamReference(stream: fileStream);
+
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ RegisterDI.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader
+{
+    public static class RegisterDI
+    {
+        public static IServiceCollection AddFileDownloder(this IServiceCollection services)
+        {
+            services.AddScoped<Downloader>();
+
+            return services;
+        }
+    }
+}
